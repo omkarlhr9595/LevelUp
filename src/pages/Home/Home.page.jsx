75a6512a444cd53b7c4eee9e8779e0f85d227f58
Omkar Lohar <omkarlohar91@gmail.com>
2023-04-30 12:04:04 +0530

    test 2

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -7,20 +7,21 @@ const Home = () => {
   const ref = useRef(null);
   useEffect(() => {
     document.title = "Level Up | Home";
-     const tl = gsap.timeline({ repeat: -1, yoyo: true });
-     tl.to(ref.current, { scale: 1.1, duration: 0.5 }).to(ref.current, {
-       scale: 1,
-       duration: 0.5,
-     });
+    const tl = gsap.timeline({ repeat: -1, yoyo: true });
+    tl.to(ref.current, { scale: 1.1, duration: 0.5 }).to(ref.current, {
+      scale: 1,
+      duration: 0.5,
+    });
   }, []);
   return (
     <div className="h-screen w-full bg-[#242424] flex flex-col justify-center items-center">
       <img
         ref={ref}
         src={logo}
-        className="h-96 p-0 transition-[filter_300ms] hover:drop-shadow-[0_0_2em_#646cffaa]"
+        className="h-96 p-0  transition-[filter_300ms] hover:drop-shadow-[0_0_2em_#646cffaa]"
         alt=""
       />
+      <h1 className="text-4xl text-white">One Place For All Your Gigs</h1>
     </div>
   );
 };

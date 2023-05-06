7113ad15b3bf05e06c8fb645d7c7966aabf8a09b
Omkar Lohar <omkarlohar91@gmail.com>
2023-05-06 18:00:09 +0530

    add: stable UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./node_modules/flowbite-react/**/*.js",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     fontFamily: {
       display: ["Abril Fatface", "cursive"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1a56db",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
